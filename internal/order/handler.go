8f3c99349a83df289705e1a3777adc4623d4fcf0
Meerthika <meerthikasrrajendrans@gmail.com>
2025-06-21 19:53:24 +0530

    [Add] orderHandler function in Handler

---
+++ internal/order/handler.go
@@ -0,0 +1,31 @@
+package order
+
+import(
+	"net/http"
+	"strconv"
+)
+
+func OrderHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method == http.MethodGet{
+		http.ServeFile(w, r, "web/index.html")
+		return
+	}
+
+	if r.Method == http.MethodPost{
+		err:= r.ParseForm();
+		if err!=nil{
+			http.Error(w, "Parse Error", http.StatusBadRequest)
+			return
+		}
+
+		product:= r.FormValue("product")
+		quantity, _:= strconv.Atoi(r.FormValue("quantity"))
+		order:= Order{
+			Product: product,
+			Quantity: quantity,
+		}
+
+		ProduceOrder(order)
+		w.Write([]byte("Order Placed!"))
+	}
+}
